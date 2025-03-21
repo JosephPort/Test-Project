2253c2d2624386d81fb2f718652af455412663b3
Joseph Porter <josephporter200@gmail.com>
2025-03-21 02:24:14 -0400

    Added class library and reference for backend

--- Test-Project.Server/Test-Project.Server.API/obj/Debug/net8.0/Test-Project.Server.API.AssemblyInfo.cs
+++ Test-Project.Server/Test-Project.Server.API/obj/Debug/net8.0/Test-Project.Server.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test-Project.Server.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda4d4c37db576139a0bd4e171f10757c21e4a8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test-Project.Server.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test-Project.Server.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
