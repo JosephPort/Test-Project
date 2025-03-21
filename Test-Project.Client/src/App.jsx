cda4d4c37db576139a0bd4e171f10757c21e4a8a
Joseph Porter <josephporter200@gmail.com>
2025-03-21 02:18:14 -0400

    Added more pages for client template

--- Test-Project.Client/src/App.jsx
+++ Test-Project.Client/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages/Home'
+import Register from './pages/auth/Register'
+import Login from './pages/auth/Login'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+
+        <Route path="auth">
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   )
