310477a5ddc9e6390410aac11f7b8713651fc5e3
Joseph Porter <josephporter200@gmail.com>
2025-03-21 02:52:42 -0400

    Add LoginDTO and RegisterDTO classes; include BCrypt.Net-Next package reference; Added register endpoint

---
+++ Test-Project.Server/Test-Project.Server.API/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+using BCrypt.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Test_Project.Server.API.Context;
+using Test_Project.Server.Models.DTOs;
+using Test_Project.Server.Models.Tables;
+
+namespace Test_Project.Server.API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly EfContext _context;
+
+        public AuthController(EfContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDTO request)
+        {
+            var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username || u.Email == request.Email);
+
+            if (dbUser is not null)
+            {
+                return BadRequest("User already exists with this username or email.");
+            }
+
+            var user = new User
+            {
+                Username = request.Username,
+                Password = BCrypt.Net.BCrypt.HashPassword(request.Password),
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName
+            };
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
